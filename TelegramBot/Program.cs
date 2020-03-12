5eefc3bfae91f363daafb5a38b369224dc863218
andriy22 <istep.andriy@gmail.com>
2020-03-12 23:13:59 +0200

    Add project files.

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        private static readonly string token = "Your Token";
+        private static readonly TelegramBotClient bot = new TelegramBotClient(token);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
