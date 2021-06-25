577d94d4f1096016e94057afc0ba038bc579d060
Andriy <istep.andriy@gmail.com>
2021-06-25 16:10:35 +0300

    Update GlobalConstants.cs

--- TelegramBot/Constants/GlobalConstants.cs
+++ TelegramBot/Constants/GlobalConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class GlobalConstants
     {
-        public const string TelegramToken = "1875547074:AAFTgTB3G2FCqAGX2yk3y2DQHzILRCi_zws";
+        public const string TelegramToken = "YourToken";
         public static string[] SupportedTypes = { "text" };
     }
 }
